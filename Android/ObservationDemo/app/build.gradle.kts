f82f8ee819dc99367dea8e7b6e009655553890e9
Julia Makeeva <bulantsevajo@gmail.com>
2025-12-10 16:39:38 +0700

    Updated gradle version in Observable example.

--- Android/ObservationDemo/app/build.gradle.kts
+++ Android/ObservationDemo/app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 
-    id("io.scade.gradle.plugins.android.swiftpm") version "1.4.2"
+    id("io.scade.gradle.plugins.android.swiftpm") version "latest.release"
 }
 
 android {
