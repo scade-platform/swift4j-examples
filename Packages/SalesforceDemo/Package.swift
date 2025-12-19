8c1716b743707d62c60bf9d6d6b5ba016a2fc971
Julia Makeeva <bulantsevajo@gmail.com>
2025-12-19 15:58:58 +0700

    Updated Package.swift.

--- Packages/SalesforceDemo/Package.swift
+++ Packages/SalesforceDemo/Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "SalesforceDemo",
     platforms: [
-        .macOS(.v12)
+        .macOS(.v12),
+        .iOS(.v18)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
